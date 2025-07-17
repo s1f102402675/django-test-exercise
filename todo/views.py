69b979a92074465457cfd740eb551ce603619ba9
1f10240139 <s1f102401393@iniad.org>
2025-07-17 14:11:17 +0900

    models.pyのさらなる編集とmakemigrationsの実行

--- todo/views.py
+++ todo/views.py
@@ -9,7 +9,8 @@ from todo.models import Task
 def index(request):
     if request.method == 'POST':
         task = Task(title=request.POST['title'],
-                    due_at=make_aware(parse_datetime(request.POST['due_at'])))
+                    due_at=make_aware(parse_datetime(request.POST['due_at'])),
+                    body=request.POST['body'])
         task.save()
 
     if request.GET.get('order') == 'due':
@@ -42,6 +43,7 @@ def update(request, task_id):
         raise Http404('Task does not exist')
     if request.method == 'POST':
         task.title = request.POST['title']
+        task.body = request.POST['body']
         task.due_at = make_aware(parse_datetime(request.POST['due_at']))
         task.save()
         return redirect(detail, task_id)
